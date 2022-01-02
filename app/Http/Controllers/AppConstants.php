3d66c6a92e160d39d3b45d1e5368dc89eba8c724
robinNcode <msmrobin518@gmail.com>
2022-01-03 02:10:48 +0600

    first api call done

--- app/Http/Controllers/AppConstants.php
+++ app/Http/Controllers/AppConstants.php
@@ -10,10 +10,12 @@ class AppConstants extends Controller
      * @var string
      */
     public $TAB_STUDENT_CLASSES;
+    public $TAB_STUDENT_ROLL;
 
     public function __construct()
     {
         $this->TAB_STUDENT_CLASSES = 'student_classes';
+        $this->TAB_STUDENT_ROLL = 'student_roll';
     }
-    
+
 }
--- app/Http/Controllers/StudentClassesController.php
+++ app/Http/Controllers/StudentClassesController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\StudentClasses;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 
